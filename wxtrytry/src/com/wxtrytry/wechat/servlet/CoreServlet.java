561418ca8a88a9ea74109bee2f74255d103e6732
SparkPan <sparkpanjun@gmail.com>
2018-05-07 18:42:56 +0800

    added java doc to SignUtil

--- wxtrytry/src/com/wxtrytry/wechat/servlet/CoreServlet.java
+++ wxtrytry/src/com/wxtrytry/wechat/servlet/CoreServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.wxtrytry.wechat.util.SignUtil;
 
-public class CoreServlet {
+public class CoreServlet extends HttpServlet {
 
+        private static  final long serialVersionUID
 }
--- wxtrytry/src/com/wxtrytry/wechat/util/SignUtil.java
+++ wxtrytry/src/com/wxtrytry/wechat/util/SignUtil.java
@@ -1,4 +1,24 @@
 package com.wxtrytry.wechat.util;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
 public class SignUtil {
+    private static String token = "SpArKaRoNsHaRkArOn";//token that wechat requires
+    /**
+     * check the signature if the information is from Tencent server
+     * @param signature the encoded information Tencent offers
+     * @param timestamp the encoded information of the request time
+     * @param nonce a random number
+     * @return if the signature is effective
+     * @throws NoSuchAlgorithmException when a particular cryptographic algorithm is requested but is not available in the environment.
+     */
+    public static boolean checkSignature(String signature, String timestamp,String nonce){
+        // sort the params in Lexicographical oder
+        String[] arr = new String[] { token, timestamp , nonce };
+        Arrays.sort(arr);
+
+    }
+
 }
