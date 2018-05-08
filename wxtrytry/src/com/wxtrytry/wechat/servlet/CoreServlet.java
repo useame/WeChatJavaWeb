a339f6131f10b1c994ccb0d13a630c6870a930f0
SparkPan <sparkpanjun@gmail.com>
2018-05-08 13:49:01 +0800

    finished the coreServlet

--- wxtrytry/src/com/wxtrytry/wechat/servlet/CoreServlet.java
+++ wxtrytry/src/com/wxtrytry/wechat/servlet/CoreServlet.java
@@ -11,5 +11,44 @@ import com.wxtrytry.wechat.util.SignUtil;
 
 public class CoreServlet extends HttpServlet {
 
-        private static  final long serialVersionUID
+    private static final long serialVersionUID = -3990430699254259022L;// to identify versions
+
+    /**
+     * to check whether the request is from WeChat Server or not
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+
+        String signature = request.getParameter("signature");
+        String timestamp = request.getParameter("timestamp");
+        String nonce = request.getParameter("nonce");
+        String echostr = request.getParameter("echostr");
+
+        PrintWriter out = response.getWriter();
+        // if the signature passes our test, we return echostr as it is
+
+        if(SignUtil.checkSignature(signature,timestamp,nonce)) {
+            out.print(echostr);
+        }
+
+        out.close();
+        out = null;
+    }
+
+    /**
+     * do with the request we get
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void doPost(HttpServletRequest request,HttpServletResponse response)
+        throws ServletException,IOException {
+        // TODO what we should do later
+    }
+
 }
