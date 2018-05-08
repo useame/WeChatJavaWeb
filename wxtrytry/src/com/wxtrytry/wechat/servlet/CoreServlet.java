395b524aa7e2754edf1d1f52f42ff77da790376f
SparkPan <sparkpanjun@gmail.com>
2018-05-08 16:09:31 +0800

    completed web.xml and defined servlet

--- wxtrytry/src/com/wxtrytry/wechat/servlet/CoreServlet.java
+++ wxtrytry/src/com/wxtrytry/wechat/servlet/CoreServlet.java
@@ -3,12 +3,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.wxtrytry.wechat.util.SignUtil;
 
+@WebServlet("/coreServlet")
 public class CoreServlet extends HttpServlet {
 
     private static final long serialVersionUID = -3990430699254259022L;// to identify versions
